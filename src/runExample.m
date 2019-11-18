96bedc381c450e28583393ded90db22fd3575f40
Andre Furlan <ensismoebius@gmail.com>
2019-11-18 16:40:31 -0300

    Limpeza de leve

--- src/runExample.m
+++
@@ -1,9 +0,0 @@
-function runExample(image)
-	%% Generate masked image
-	[maskedRgbImage, mask] = generateMaskedImage(image);
-
-	%% Shows results
-	subplot(2,2,1),imshow(image);
-	subplot(2,2,3),imshow(maskedRgbImage);
-	subplot(2,2,4),imshow(mask);
-end
