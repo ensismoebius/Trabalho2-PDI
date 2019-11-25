a3d4e70788e5768b418e900a71f444335f409384
hiagobrajato <hiagobrajato123@gmail.com>
2019-11-25 19:54:03 -0300

    últimos ajustes - pastas com imagens foram colocadas no caminho já

--- src/application.m
+++ src/application.m
@@ -2,8 +2,8 @@
 kfoldsAmount=5;
 
 %calling the function for reading the folder of images
-maligna_images= image_reader('.\samples\Roi_Recort_bisque_Maligna');
-benigna_images= image_reader('.\samples\Roi_Recort_bisque_Benigna');
+maligna_images= image_reader('Roi_Recort_bisque_Maligna');
+benigna_images= image_reader('Roi_Recort_bisque_Benigna');
 
 %filtering the images (choose % of the total) -- 1 == all images
 maligna_images= filtering_images(maligna_images, 1);
